42a1f9ea24beec5ea1892b82d71b97a48dc360e1
Collin Jilbert <cjilbert504@gmail.com>
2019-10-09 20:17:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,8 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
 	{:railroads=>{:pieces=> 4,
-		 :rent_in_dollars=>{:first_level=>{}, :second_level=>{}, :third_level=>{}, :fourth_level=>{}}
+		 :rent_in_dollars=>{:first_level=>{}, :second_level=>{}, :third_level=>{}, :fourth_level=>{}},
+		 :names=>{}
 	}
 }
 end
