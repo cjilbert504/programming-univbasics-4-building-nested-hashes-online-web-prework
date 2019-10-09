21cea9839e16d1f9ecb1b10a8022f33c03cf8faf
Collin Jilbert <cjilbert504@gmail.com>
2019-10-09 20:22:34 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,7 +19,7 @@ def monopoly_with_third_tier
   # complexity.
 	{:railroads=>{
 		 :pieces=> 4,
-		 :rent_in_dollars=>{:one_piece_owned=>25, :second_level=>{}, :third_level=>{}, :fourth_level=>{}},
+		 :rent_in_dollars=>{:one_piece_owned=>25, :two_pieces_owned=>50, :third_level=>{}, :fourth_level=>{}},
 		 :names=>{:first_name=>{},:second_name=>{},:third_name=>{},:fourth_name=>{}}
 	}
 }
